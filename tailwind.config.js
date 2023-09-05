2ffd95c44318199a0c5eda22259ba4e678e3b5bd
Pilks-pixel <peterpilkington@hotmail.com>
2023-09-05 19:16:56 +0200

    talewind base

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				display: "Bricolage Grotesque, sans-serif",
+				body: "Nunito",
+			},
+		},
+	},
+	plugins: [],
+};
 
