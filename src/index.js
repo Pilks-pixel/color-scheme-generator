375598857e0c1cdcac0d9a5d527ef30491ce1663
Pilks-pixel <peterpilkington@hotmail.com>
2023-09-06 17:58:29 +0200

    submit event listener

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
+// First Function
 var firstType = function (person) {
     console.log("Hello ".concat(person, " !"));
 };
-firstType('Pete');
+firstType("Pete");
+// Form Functions
+var selectedColor = function (e) {
+    e.preventDefault();
+    var colorPickerTarget = e.target;
+    console.log(colorPickerTarget.value);
+};
+var handleSubmit = function (e) {
+    e.preventDefault();
+    var colorPickerTarget = e.target;
+    var modeSelector = colorPickerTarget.mode.value;
+    var colorSelected = colorPickerTarget.seedColor.value.slice(2);
+    fetch("https://www.thecolorapi.com/scheme?hex=".concat(colorSelected, "&mode=").concat(modeSelector, "&count=6"))
+        .then(function (res) { return res.json(); })
+        .then(function (data) {
+        console.log(data.colors);
+    });
+};
+// Event Listeners
+var colorPicker = document.getElementById("seedColor");
+var colorForm = document.getElementById("colorForm");
+colorPicker.addEventListener("change", selectedColor);
+colorForm.addEventListener("submit", handleSubmit);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,40 @@
-const firstType = (person : string) => {
-    console.log(`Hello ${person} !`)
+// First Function
+const firstType = (person: string) => {
+	console.log(`Hello ${person} !`);
 };
 
-firstType('Pete');
+firstType("Pete");
+
+// Form Functions
+const selectedColor = (e: Event) => {
+	e.preventDefault();
+	const colorPickerTarget = e.target as HTMLInputElement;
+	console.log(colorPickerTarget.value);
+};
+
+const handleSubmit = (e: Event) => {
+	e.preventDefault();
+	const colorPickerTarget = e.target as HTMLFormElement;
+	const modeSelector = colorPickerTarget.mode.value as HTMLInputElement;
+	const colorSelected = colorPickerTarget.seedColor.value.slice(
+		2
+	) as HTMLInputElement;
+
+
+	fetch(
+		`https://www.thecolorapi.com/scheme?hex=${colorSelected}&mode=${modeSelector}&count=6`
+	)
+		.then(res => res.json())
+		.then(data => {
+            console.log(data.colors)
+            
+        });
+};
+
+// Event Listeners
+const colorPicker = document.getElementById("seedColor") as HTMLInputElement;
+
+const colorForm = document.getElementById("colorForm") as HTMLFormElement;
+
+colorPicker.addEventListener("change", selectedColor);
+colorForm.addEventListener("submit", handleSubmit);
