2579a3cc3aa9c40527dd24fd50e95c15c04b21bc
Pilks-pixel <peterpilkington@hotmail.com>
2023-09-10 20:32:27 +0200

    refactor

--- public/index.js
+++ public/index.js
@@ -4,30 +4,36 @@ var selectedColor = function (e) {
     var colorPickerTarget = e.target;
     console.log(colorPickerTarget.value.slice(1));
 };
+var getSchemeColors = function (color, mode) {
+    fetch("https://www.thecolorapi.com/scheme?hex=".concat(color, "&mode=").concat(mode, "&count=6"))
+        .then(function (res) { return res.json(); })
+        .then(function (data) { return setNewScheme(data.colors); });
+};
+var setNewScheme = function (scheme) {
+    var colorElements = scheme.map(function (c, index) {
+        return "<div id=\"colorPallet".concat(index, "\"  class=\"p-4 w-full\" style=\"background-color:").concat(c.hex.value, ";\">").concat(c.hex.value, "</div>");
+    });
+    schemeContainer.innerHTML = colorElements.join("");
+    var colorPallet = document.getElementById("colorPallet1");
+    colorPallet.addEventListener("click", handleClick);
+};
 var handleSubmit = function (e) {
     e.preventDefault();
     var colorPickerTarget = e.target;
-    var modeSelector = colorPickerTarget.mode.value;
+    var modeSelected = colorPickerTarget.mode.value;
     var colorSelected = colorPickerTarget.seedColor.value.slice(1);
-    fetch("https://www.thecolorapi.com/scheme?hex=".concat(colorSelected, "&mode=").concat(modeSelector, "&count=6"))
-        .then(function (res) { return res.json(); })
-        .then(function (data) {
-        var scheme = data.colors;
-        var schemeContainer = document.getElementById("scheme_container");
-        var schemeArr = scheme.map(function (c, index) {
-            return "<div id=\"colorPallet".concat(index, "\"  class=\"p-4 w-full\" style = \"background-color:").concat(c.hex.value, ";\">").concat(c.hex.value, "</div>");
-        });
-        console.table(schemeArr);
-        schemeContainer.innerHTML = schemeArr.join("");
-    });
+    getSchemeColors(colorSelected, modeSelected);
 };
 // Color Pallet Functions
-var handleClick = function (e) {
-    console.log(e.target);
-};
+function handleClick(e) {
+    e.preventDefault();
+    console.log(e.currentTarget);
+}
+;
 // Event Listeners
 var colorPicker = document.getElementById("seedColor");
 var colorForm = document.getElementById("colorForm");
+var schemeContainer = document.getElementById("scheme_container");
 colorPicker.addEventListener("change", selectedColor);
 colorForm.addEventListener("submit", handleSubmit);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 // Form Functions
 const selectedColor = (e: Event) => {
 	e.preventDefault();
@@ -6,45 +5,50 @@ const selectedColor = (e: Event) => {
 	console.log(colorPickerTarget.value.slice(1));
 };
 
+
+const getSchemeColors = (color: HTMLInputElement, mode: HTMLInputElement) => {
+ 	fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}&count=6`)
+		.then(res => res.json())
+		.then(data => setNewScheme(data.colors))
+};
+
+const setNewScheme = (scheme : {}[]) => {
+	const colorElements = scheme.map((c: any, index: number) => {
+		return `<div id="colorPallet${index}"  class="p-4 w-full" style="background-color:${c.hex.value};">${c.hex.value}</div>`;
+	});
+
+   schemeContainer.innerHTML = colorElements.join("");
+   const colorPallet = document.getElementById("colorPallet1");
+		colorPallet.addEventListener("click", handleClick);
+
+
+}
+
 const handleSubmit = (e: Event) => {
 	e.preventDefault();
 	const colorPickerTarget = e.target as HTMLFormElement;
-	const modeSelector = colorPickerTarget.mode.value as HTMLInputElement;
+	const modeSelected = colorPickerTarget.mode.value as HTMLInputElement;
 	const colorSelected = colorPickerTarget.seedColor.value.slice(
 		1
 	) as HTMLInputElement;
 
-
-	fetch(
-		`https://www.thecolorapi.com/scheme?hex=${colorSelected}&mode=${modeSelector}&count=6`
-	)
-		.then(res => res.json())
-		.then((data) => {
-            const scheme = data.colors
-			const schemeContainer = document.getElementById("scheme_container");
-			
-            const schemeArr : [] = scheme.map((c: any, index : string)  => {
-				return `<div id="colorPallet${index}"  class="p-4 w-full" style = "background-color:${c.hex.value};">${c.hex.value}</div>`;	
-					
-				
-			});
-			console.table(schemeArr);
-			schemeContainer.innerHTML = schemeArr.join("");
-
-			
-
-        });
+	getSchemeColors(colorSelected,modeSelected)
 };
 
+
+
+
 // Color Pallet Functions
-const handleClick = (e: Event) => {
-	console.log(e.target)
+ function handleClick(e: Event) {
+	e.preventDefault();
+	console.log(e.currentTarget);
 };
 
 // Event Listeners
 const colorPicker = document.getElementById("seedColor") as HTMLInputElement;
-
 const colorForm = document.getElementById("colorForm") as HTMLFormElement;
+const schemeContainer = document.getElementById("scheme_container");
 
 colorPicker.addEventListener("change", selectedColor);
 colorForm.addEventListener("submit", handleSubmit);
+
