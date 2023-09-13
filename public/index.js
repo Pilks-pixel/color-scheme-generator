0ab319fc03b3ba8fd5b337a2b3a7c4dc98cf3a14
Pilks-pixel <peterpilkington@hotmail.com>
2023-09-13 18:24:02 +0200

    dark mode toggle

--- public/index.js
+++ public/index.js
@@ -1,9 +1,4 @@
 // Form Functions
-var selectedColor = function (e) {
-    e.preventDefault();
-    var colorPickerTarget = e.target;
-    console.log(colorPickerTarget.value.slice(1));
-};
 var getSchemeColors = function (color, mode) {
     fetch("https://www.thecolorapi.com/scheme?hex=".concat(color, "&mode=").concat(mode, "&count=6"))
         .then(function (res) { return res.json(); })
@@ -11,7 +6,7 @@ var getSchemeColors = function (color, mode) {
 };
 var setNewScheme = function (scheme) {
     var colorElements = scheme.map(function (c) {
-        return "<div data-value=\"".concat(c.hex.value, "\"  class=\"p-4 w-full tooltip .tooltip:active\"  style=\"background-color:").concat(c.hex.value, ";\">").concat(c.hex.value, "</div>");
+        return "<div data-value=\"".concat(c.hex.value, "\"  class=\"p-4 w-full tooltip tooltip:active\"  style=\"background-color:").concat(c.hex.value, ";\">").concat(c.hex.value, "</div>");
     });
     schemeContainer.innerHTML = colorElements.join("");
 };
@@ -21,21 +16,44 @@ var handleSubmit = function (e) {
     var modeSelected = colorPickerTarget.mode.value;
     var colorSelected = colorPickerTarget.seedColor.value.slice(1);
     getSchemeColors(colorSelected, modeSelected);
+    colorForm.reset();
 };
-// Color Pallet Functions
+// Color Copy To Clipboard Function
 function handleClick(e) {
-    e.preventDefault();
     var schemeElement = e.target;
     var schemeColor = schemeElement.dataset.value;
     console.log(schemeColor);
     navigator.clipboard.writeText(schemeColor);
 }
-;
+// Dark Mode Theme
+var currentTheme = localStorage.getItem("theme");
+var rootElement = document.documentElement.classList;
+var handleThemeToggle = function () {
+    currentTheme = localStorage.getItem("theme");
+    if (currentTheme === "light") {
+        enableDarkMode();
+    }
+    else {
+        enableLightMode();
+    }
+};
+var enableDarkMode = function () {
+    rootElement.add("dark");
+    localStorage.setItem("theme", "dark");
+    darkModeToggle.innerHTML = "<i class=\"fa-regular fa-sun\"></i>";
+};
+var enableLightMode = function () {
+    rootElement.remove("dark");
+    localStorage.setItem("theme", "light");
+    darkModeToggle.innerHTML = "<i class='fa-regular fa-moon'></i>";
+};
 // Event Listeners
-var colorPicker = document.getElementById("seedColor");
 var colorForm = document.getElementById("colorForm");
 var schemeContainer = document.getElementById("scheme_container");
-colorPicker.addEventListener("change", selectedColor);
+var darkModeToggle = document.getElementById("toggle_btn");
 colorForm.addEventListener("submit", handleSubmit);
 schemeContainer.addEventListener("click", handleClick);
+darkModeToggle.addEventListener("click", handleThemeToggle);
+// Set Theme On Page Load
+currentTheme === "light" ? enableLightMode() : enableDarkMode();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,17 @@
 // Form Functions
-const selectedColor = (e: Event) => {
-	e.preventDefault();
-	const colorPickerTarget = e.target as HTMLInputElement;
-	console.log(colorPickerTarget.value.slice(1));
-};
-
-
 const getSchemeColors = (color: HTMLInputElement, mode: HTMLInputElement) => {
- 	fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}&count=6`)
+	fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}&count=6`)
 		.then(res => res.json())
-		.then(data => setNewScheme(data.colors))
+		.then(data => setNewScheme(data.colors));
 };
 
-const setNewScheme = (scheme : {}[]) => {
+const setNewScheme = (scheme: {}[]) => {
 	const colorElements = scheme.map((c: any) => {
-		return `<div data-value="${c.hex.value}"  class="p-4 w-full tooltip .tooltip:active"  style="background-color:${c.hex.value};">${c.hex.value}</div>`;
+		return `<div data-value="${c.hex.value}"  class="p-4 w-full tooltip tooltip:active"  style="background-color:${c.hex.value};">${c.hex.value}</div>`;
 	});
 
 	schemeContainer.innerHTML = colorElements.join("");
-}
+};
 
 const handleSubmit = (e: Event) => {
 	e.preventDefault();
@@ -28,28 +21,58 @@ const handleSubmit = (e: Event) => {
 		1
 	) as HTMLInputElement;
 
-	getSchemeColors(colorSelected,modeSelected)
+	getSchemeColors(colorSelected, modeSelected);
+	colorForm.reset();
 };
 
-
-// Color Pallet Functions
- function handleClick(e: Event) {
-	e.preventDefault();
+// Color Copy To Clipboard Function
+function handleClick(e: Event) {
 	const schemeElement = e.target as HTMLElement;
-	const schemeColor = schemeElement.dataset.value
-	console.log(schemeColor)
+	const schemeColor = schemeElement.dataset.value;
+	console.log(schemeColor);
 	navigator.clipboard.writeText(schemeColor);
+}
+
+// Dark Mode Theme
+let currentTheme = localStorage.getItem("theme");
+const rootElement = document.documentElement.classList;
+
+const handleThemeToggle = () => {
+	currentTheme = localStorage.getItem("theme");
+
+	if (currentTheme === "light") {
+		enableDarkMode()
+	} else {
+		enableLightMode();
+	}
 };
+
+const enableDarkMode = () => {
+	rootElement.add("dark");
+	localStorage.setItem("theme", "dark");
+	darkModeToggle.innerHTML = `<i class="fa-regular fa-sun"></i>`;
+
+};
+
+const enableLightMode = () => {
+	rootElement.remove("dark");
+	localStorage.setItem("theme", "light");
+	darkModeToggle.innerHTML = `<i class='fa-regular fa-moon'></i>`;
+
+};
+
 
 // Event Listeners
-const colorPicker = document.getElementById("seedColor") as HTMLInputElement;
 const colorForm = document.getElementById("colorForm") as HTMLFormElement;
 const schemeContainer = document.getElementById("scheme_container");
+const darkModeToggle = document.getElementById(
+	"toggle_btn"
+	) as HTMLButtonElement;
+	
+	colorForm.addEventListener("submit", handleSubmit);
+	schemeContainer.addEventListener("click", handleClick);
+	darkModeToggle.addEventListener("click", handleThemeToggle);
 
-
-colorPicker.addEventListener("change", selectedColor);
-colorForm.addEventListener("submit", handleSubmit);
-schemeContainer.addEventListener("click", handleClick);
-
-
+// Set Theme On Page Load
+currentTheme === "light" ? enableLightMode() : enableDarkMode();
 
